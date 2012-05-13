4f4e873ba92738d2b9bcf82c5c595033b93a949c
men0x <men0x@kabal>
2012-05-13 17:40:54 +0200

    impl upload routine

--- src/main/java/de/htwkleipzig/mmdb/mvc/controller/UploadController.java
+++ src/main/java/de/htwkleipzig/mmdb/mvc/controller/UploadController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import de.htwkleipzig.mmdb.model.UploadItem;
+import de.htwkleipzig.mmdb.util.FileUploader;
 
 /**
  * @author spinner0815
@@ -22,28 +23,32 @@ import de.htwkleipzig.mmdb.model.UploadItem;
 @RequestMapping(value = "/upload")
 public class UploadController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(UploadController.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(UploadController.class);
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String getUploadForm(Model model) {
-        model.addAttribute(new UploadItem());
-        return "uploadForm";
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public String getUploadForm(Model model) {
+		model.addAttribute(new UploadItem());
+		return "uploadForm";
+	}
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String create(UploadItem uploadItem, BindingResult result) {
-        if (result.hasErrors()) {
-            for (ObjectError error : result.getAllErrors()) {
-                LOGGER.error("Error: " + error.getCode() + " - " + error.getDefaultMessage());
-            }
-            return "uploadForm";
-        }
+	@RequestMapping(method = RequestMethod.POST)
+	public String create(UploadItem uploadItem, BindingResult result) {
+		if (result.hasErrors()) {
+			for (ObjectError error : result.getAllErrors()) {
+				LOGGER.error("Error: " + error.getCode() + " - "
+						+ error.getDefaultMessage());
+			}
+			return "uploadForm";
+		}
 
-        // Some type of file processing...
-        LOGGER.debug("-------------------------------------------");
-        LOGGER.debug("Test upload: " + uploadItem.getName());
-        LOGGER.debug("Test upload: " + uploadItem.getFileData().getOriginalFilename());
-        LOGGER.debug("-------------------------------------------");
-        return "redirect:/";
-    }
+		// Some type of file processing...
+		LOGGER.debug("-------------------------------------------");
+		LOGGER.debug("Test upload: " + uploadItem.getName());
+		LOGGER.debug("Test upload: "
+				+ uploadItem.getFileData().getOriginalFilename());
+		new FileUploader(uploadItem);
+		LOGGER.debug("-------------------------------------------");
+		return "redirect:/";
+	}
 }
