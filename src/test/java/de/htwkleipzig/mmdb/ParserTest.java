a3f63e5aaec06bf127948db31703e7398f712655
Lars Wesemann <lars.wesemann@googlemail.com>
2012-05-28 14:12:35 +0200

    some tests were deprecated, delete

--- src/test/java/de/htwkleipzig/mmdb/ParserTest.java
+++ src/test/java/de/htwkleipzig/mmdb/ParserTest.java
@@ -1,22 +1,11 @@
 package de.htwkleipzig.mmdb;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import de.htwkleipzig.mmdb.service.PaperService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/spring/integration/spring-integration-context.xml" })
 public class ParserTest {
 
-    @Autowired
-    PaperService paperService;
-
-    @Test
-    public void testPdfParser() {
-        paperService.delete("809fe0e79e055d35ef11f6aedf83eff6");
-    }
 }
