36b4340c4e19bd3ab038ab2dfddb39dec5f319a2
Lars Wesemann <lars.wesemann@googlemail.com>
2012-06-09 10:46:32 +0200

    filedownloader, with instpiration from Oliver, implemented. Also began a versioning of the project

--- src/main/java/de/htwkleipzig/mmdb/mvc/controller/PaperController.java
+++ src/main/java/de/htwkleipzig/mmdb/mvc/controller/PaperController.java
@@ -1,5 +1,13 @@
 package de.htwkleipzig.mmdb.mvc.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +15,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import de.htwkleipzig.mmdb.model.Paper;
 import de.htwkleipzig.mmdb.service.PaperService;
+import de.htwkleipzig.mmdb.util.Utilities;
 
 /**
  * @author spinner0815
@@ -33,5 +43,22 @@ public class PaperController {
         Paper paper = paperService.get(recievedPaperName);
         model.addAttribute("paperName", paper.getFileName());
         return "paper";
+    }
+
+    @RequestMapping(value = "/download")
+    public String downloadFile(@RequestParam("paperId") String paperId, HttpServletResponse resp) throws IOException {
+        String paperDirectory = Utilities.getProperty("paper.directory");
+        LOGGER.debug("files loading from {}", paperDirectory);
+        File file = new File(paperDirectory + "/" + paperId + ".pdf");
+
+        resp.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
+        OutputStream out = resp.getOutputStream();
+        resp.setContentType("text/plain; charset=utf-8");
+        FileInputStream fi = new FileInputStream(file);
+        IOUtils.copy(fi, out);
+        out.flush();
+        out.close();
+
+        return null;
     }
 }
